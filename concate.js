ac53ba608a31d487ff845c0b6f69d184a5e8f11a
nikhilroy2 <nikhilchandraroypoet@gmail.com>
2026-06-22 07:37:43 +0600

    update concate

--- concate.js
+++ concate.js
@@ -1,5 +1,5 @@
-//let p = document.getElementById('p');
-//p.innerHTML = ""+p.innerHTML+" App" // string concate
+let p = document.getElementById('p');
+p.innerHTML = ""+p.innerHTML+" App" // string concate
     //`${p.innerText} App` // template literal
     // //p.innerText + ' App'  // concate
     // //"Hello World App" // normal
