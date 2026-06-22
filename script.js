f746ef09e62e1648c760bc90693b81e67fd5a3b3
nikhilroy2 <nikhilchandraroypoet@gmail.com>
2026-06-22 07:36:40 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+//let p = document.getElementById('p');
+//p.innerHTML = ""+p.innerHTML+" App" // string concate
+    //`${p.innerText} App` // template literal
+    // //p.innerText + ' App'  // concate
+    // //"Hello World App" // normal
